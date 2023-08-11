ffdddf7752512982993126267c7525afeade9408
y6ec813 <balaram4chat@gmail.com>
2023-08-11 17:27:47 +1000

    feat: Add security group

--- main.tf
+++ main.tf
@@ -3,10 +3,31 @@ provider "aws" {
 }
 
 resource "aws_instance" "terraform-ec2-instance" {
-  ami           = "ami-0a709bebf4fa9246f"
+  ami           = "ami-0310483fb2b488153" //Ubuntu AMI
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+                #! /bin/bash
+                echo "Hello World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+
+    user_data_replace_on_change = true
 
   tags = {
     Name = "terraform-example"
   }
+}
+
+
+resource "aws_security_group" "instance" {
+    name = "terraform-sg-httpd"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
